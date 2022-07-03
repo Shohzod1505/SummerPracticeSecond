25f7dc5ee714c18c89e9d301c412d8460e3685d7
Shohzod1505 <92218360+Shohzod1505@users.noreply.github.com>
2022-07-03 13:10:26 +0300

    Delete main/java/com/example/summerpracticethird directory

--- main/java/com/example/summerpracticethird/Country.kt
+++
@@ -1,6 +0,0 @@
-package com.example.summerpracticethird
-
-data class Country(val id: Int,
-                   val name: String,
-                   val flag: Int,
-                   val description: String,)
